7cac667102440b8d5f17264e3ced5a137202aa7d
EwelinaTester <118475213+EwelinaTester@users.noreply.github.com>
2023-01-09 19:55:52 +0100

    Add new Python file

---
+++ Guess the number.py
@@ -0,0 +1,17 @@
+# A small game to guess the number invented by the computer. Try your luck! 
+
+import random
+my_number = random.randint(0,100)
+
+guess=-1
+print("Enter a number between 0 and 100")
+
+while guess != my_number:
+    guess=int(input())
+
+    if guess == my_number:
+        print("Gratulation! That's the right number :)")
+    elif guess > my_number:
+        print("Unfortunately, your number is too high :( Try again!")
+    else:
+        print("Unfortunately, your number is too small :( Try again!"),
